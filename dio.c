f6e6152bcefb433fffb08db95826e6cdacb5c53a
Jason R. Jones <jjones@updsys.com>
2016-02-03 20:41:24 -0500

    Removed pins that are not present on the device

--- dio.c
+++ dio.c
@@ -15,12 +15,6 @@ void (*DIO_p15FunctPtr)();
 void (*DIO_p16FunctPtr)();
 void (*DIO_p17FunctPtr)();
 
-void (*DIO_p20FunctPtr)();
-void (*DIO_p21FunctPtr)();
-void (*DIO_p22FunctPtr)();
-void (*DIO_p23FunctPtr)();
-void (*DIO_p24FunctPtr)();
-void (*DIO_p25FunctPtr)();
 void (*DIO_p26FunctPtr)();
 void (*DIO_p27FunctPtr)();
 
@@ -86,46 +80,8 @@ __interrupt void DIO_port1IntFunct(){
 #pragma vector=PORT2_VECTOR
 __interrupt void DIO_port2IntFunct(){
 	/* find out which pin triggered the interrupt */
-	if(P2IFG & BIT0){
-		/* if the function pointer points to an address,
-		 * then execute that function call */
-		if(DIO_p20FunctPtr != 0){
-			(*DIO_p20FunctPtr)();
-		}
-
-		P2IFG &= ~BIT0;
-	}else if(P2IFG & BIT1){
-		if(DIO_p21FunctPtr != 0){
-			(*DIO_p21FunctPtr)();
-		}
-
-		P2IFG &= ~BIT1;
-	}else if(P2IFG & BIT2){
-		if(DIO_p22FunctPtr != 0){
-			(*DIO_p22FunctPtr)();
-		}
-
-		P2IFG &= ~BIT2;
-	}else if(P2IFG & BIT3){
-		if(DIO_p23FunctPtr != 0){
-			(*DIO_p23FunctPtr)();
-		}
-
-		P2IFG &= ~BIT3;
-	}else if(P2IFG & BIT4){
-		if(DIO_p24FunctPtr != 0){
-			(*DIO_p24FunctPtr)();
-		}
-
-		P2IFG &= ~BIT4;
-	}else if(P2IFG & BIT5){
-		if(DIO_p25FunctPtr != 0){
-			(*DIO_p25FunctPtr)();
-		}
-
-		P2IFG &= ~BIT5;
-	}else if(P2IFG & BIT6){
-		if(DIO_p26FunctPtr != 0){
+if(P2IFG & BIT6){
+	if(DIO_p26FunctPtr != 0){
 			(*DIO_p26FunctPtr)();
 		}
 
@@ -136,6 +92,8 @@ __interrupt void DIO_port2IntFunct(){
 		}
 
 		P2IFG &= ~BIT7;
+	}else{
+	    while(1); // programmer's trap
 	}
 }
 
@@ -225,12 +183,6 @@ void DIO_enableInterrupt(int port, int pin, void (*functPtr)()){
 
             /* associate the function pointer */
             switch(pin){
-                case 0: DIO_p20FunctPtr = functPtr; break;
-                case 1: DIO_p21FunctPtr = functPtr; break;
-                case 2: DIO_p22FunctPtr = functPtr; break;
-                case 3: DIO_p23FunctPtr = functPtr; break;
-                case 4: DIO_p24FunctPtr = functPtr; break;
-                case 5: DIO_p25FunctPtr = functPtr; break;
                 case 6: DIO_p26FunctPtr = functPtr; break;
                 case 7: DIO_p27FunctPtr = functPtr; break;
                 default: while(1);
